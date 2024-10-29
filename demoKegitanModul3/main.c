b26ae46358ecdc247162960aa0ead9445763869d
SuryaHipersomnia <soeryaoemsoenia@SoeryaOemsoenias-MacBook-Pro.local>
2024-10-29 15:13:58 +0700

    menambahkan cpmk 4

--- demoKegitanModul3/main.c
+++ demoKegitanModul3/main.c
@@ -13,20 +13,15 @@ int main()
     int member = pilihMember();                       
     int hari = 0, bulan = 0, tahun = 0;
 
-    // Input tanggal
     inputTanggal(&hari, &bulan, &tahun); 
     line();
 
-    // Menghitung diskon
     totalHarga = applyDiscounts(totalHarga, member, hari, bulan, tahun);
 
-    // Menampilkan total harga
     printTotalHarga(totalHarga);
 
-    // Input pembayaran
     float pembayaran = inputPembayaran(totalHarga);
 
-    // Menghitung kembalian
     calculateKembalian(totalHarga, pembayaran);
 
     return 0;
